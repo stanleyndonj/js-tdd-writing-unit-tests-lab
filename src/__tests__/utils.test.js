6734c8743e4a4d69ee1ba69d7571008e05ca74f7
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-09-08 22:35:08 +0300

    complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,39 @@
 // Your tests here
+
+const { isPalindrome } = require('../utils');
+
+describe('isPalindrome function', () => {
+  test('should return true for palindromic words', () => {
+    expect(isPalindrome("racecar")).toBe(true);
+    expect(isPalindrome("mom")).toBe(true);
+    expect(isPalindrome("level")).toBe(true);
+  });
+
+  test('should return false for non-palindromic words', () => {
+    expect(isPalindrome("car")).toBe(false);
+    expect(isPalindrome("hello")).toBe(false);
+    expect(isPalindrome("world")).toBe(false);
+  });
+
+  test('should return true for words that are a combination of uppercase and lowercase letters', () => {
+    expect(isPalindrome("Racecar")).toBe(true);
+    expect(isPalindrome("Madam")).toBe(true);
+    expect(isPalindrome("LeVel")).toBe(true);
+  });
+
+  test('should return false for an empty string', () => {
+    expect(isPalindrome("")).toBe(false);
+  });
+
+  test('should throw an error if input has any non-alphabetic characters', () => {
+    expect(() => isPalindrome("12321")).toThrow();
+    expect(() => isPalindrome("hello123")).toThrow();
+    expect(() => isPalindrome("race@car")).toThrow();
+  });
+
+  test('should throw an error if input that isn\'t a string', () => {
+    expect(() => isPalindrome(12321)).toThrow();
+    expect(() => isPalindrome(true)).toThrow();
+    expect(() => isPalindrome({})).toThrow();
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,20 @@
 // Your code here
+function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error('Input must be a string');
+    }
+  
+    if (word.length === 0) {
+      return false; 
+    }
+  
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error('Input must contain only alphabetic characters');
+    }
+  
+    const cleanWord = word.toLowerCase().replace(/[^a-z]/g, '');
+    const reversedWord = cleanWord.split('').reverse().join('');
+    return cleanWord === reversedWord;
+  }
+  
+  module.exports = { isPalindrome };
